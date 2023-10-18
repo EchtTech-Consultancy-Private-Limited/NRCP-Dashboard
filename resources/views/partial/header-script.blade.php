99bca46e3f7666c9595a65af0d3afa78b79cf0d8
suraj-echttech <suraj.chauhan@echttech.com>
2023-10-18 11:42:57 +1000

    removed extra jquery

--- resources/views/partial/header-script.blade.php
+++ resources/views/partial/header-script.blade.php
@@ -2,11 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>NRCP | Dashboard</title>
 
-{{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js"
-    integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw=="
-    crossorigin="anonymous" referrerpolicy="no-referrer"></script> --}}
-
-
+<script src="{{ asset('jquery.min.js') }}"></script>
 <!-- Google Font: Source Sans Pro -->
 
 <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
