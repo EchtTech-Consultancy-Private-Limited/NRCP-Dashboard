4313a96e78ad58f3c7fbee914938e86493413392
nee7355 <86777494+nee7355@users.noreply.github.com>
2023-12-11 16:53:26 +0530

    nrcp changes

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,11 +2,11 @@
 <div class="content-wrapper">
     <div class="container-fluid">
         <div class="row mb-2 d-flex align-items-center">
-            <!-- <div class="col-sm-8">
-                <h1 class=" text-dark text-left  main-title nrcp-main-title" >State Dashboard - Human Health Rabies</h1>
-            </div>/.col -->
+            <div class="col-sm-8">
+                <h1 class=" text-dark text-left  main-title nrcp-main-title" >Dashboard for Rabies</h1>
+            </div>
             <div class="col-sm-4">
-                <ol class="breadcrumb  m-0">
+                <ol class="breadcrumb  m-0 justify-content-end">
                     <li class="breadcrumb-item"><a href="#">Home</a></li>
                     <li class="breadcrumb-item active">Dashboard</li>
                 </ol>
