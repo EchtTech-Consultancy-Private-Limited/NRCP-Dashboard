8bff073a3ffb232572f3f5f0c97c6f54875a66ed
brijesh-echttech <brijesh.sharma@echttech.com>
2024-02-19 16:03:16 +0530

    bug fix

--- resources/views/partial/header-script.blade.php
+++ resources/views/partial/header-script.blade.php
@@ -1,11 +1,9 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="csrf-token" content="{{ csrf_token() }}">
 <title>NRCP | Dashboard</title>
-
 <script src="{{ asset('jquery.js') }}"></script>
 <!-- Google Font: Source Sans Pro -->
-
-
 <link rel="stylesheet" href="{{ asset('assets/css/dashboard.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/css/print.css') }}"  media="print">
 <link href="{{asset('assets/css/select2.min.css')}}" rel="stylesheet" />
@@ -17,14 +15,10 @@
 <link rel="stylesheet" href="{{ asset('assets/login/plugins/overlayScrollbars/css/OverlayScrollbars.min.css') }}">
 <!-- Theme style -->
 <link rel="stylesheet" href="{{ asset('assets/login/dist/css/adminlte.min.css') }}">
-
 <link rel="stylesheet" href="{{ asset('assets/login/dist/css/tempusdominus-bootstrap-4.css') }}">
-
 <link rel="stylesheet" href="{{ asset('assets/login/dist/css/tempusdominus-bootstrap-4.min.css') }}">
 {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script> --}}
-
 <link rel="stylesheet" href="{{ asset('assets/pform_css/style.css') }}">
 <link rel="stylesheet" href="{{ asset('assets/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-
 <script src="https://code.highcharts.com/maps/highmaps.js"></script>
 <script src="https://code.highcharts.com/maps/modules/exporting.js"></script>
