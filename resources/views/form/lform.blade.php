84c5f2f43ad5df5dbfa02bd8d453ea48be45505e
brijesh-echttech <brijesh.sharma@echttech.com>
2024-02-28 12:32:06 +0530

    add p s form

---
+++ resources/views/form/lform.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+@section("content")
+<!-- Main content -->
+<section class="content sform">
+    <div class="container-fluid">
+        <div class="panel-body">
+        
+        </div>
+    </div>
+</section>
+@endsection
