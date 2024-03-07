ea58647a46fbd7b73226ed06998e65d51f5fa289
neeraj7355 <neeraj.chaursiya@echttech.com>
2024-03-07 15:58:37 +0530

    added accordion redesign the laboratory page card

--- resources/views/partial/sidebar.blade.php
+++ resources/views/partial/sidebar.blade.php
@@ -41,48 +41,89 @@
 <ul class="accordion side-link">
     <li>
         @if (Auth::user()->user_type == 2)
-            <div class="arrow arrow-right link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'general-laboratory')?'active':'' }}">
-                <a href="{{ url('/general-laboratory') }}"><i class="fa fa-list iconmargin-set" aria-hidden="true"></i>  General</a>
-            </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'quality-assurance')?'active':'' }}">
-                <a href="{{ url('/quality-assurance') }}"><i class="fa fa-check iconmargin-set" aria-hidden="true"></i> Quality</a> 
-            </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'equipment')?'active':'' }}">
-                <a href="{{ url('/equipment') }}"><i class="fa fa-cogs iconmargin-set" aria-hidden="true"></i> Equipment</a>
-            </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'rabies-test')?'active':'' }}">
-                <a href="{{ url('/rabies-test') }}"><i class="fa fa-registered iconmargin-set" aria-hidden="true"></i> Rabies</a> 
-            </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'expenditure')?'active':'' }}">
-                <a href="{{ url('/expenditure') }}"><i class="fa fa-expand iconmargin-set" aria-hidden="true"></i> Expenditure</a> </i>
-            </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'report-list')?'active':'' }}">
-                <a href="{{ url('/report-list') }}"><i class="fa fa-file iconmargin-set" aria-hidden="true"></i> Reports</a> </i>
-            </div>
+        <div
+            class="arrow arrow-right link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'general-laboratory')?'active':'' }}">
+            <a href="{{ url('/general-laboratory') }}"><i class="fa fa-list iconmargin-set" aria-hidden="true"></i>
+                General</a>
+        </div>
+        <div
+            class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'quality-assurance')?'active':'' }}">
+            <a href="{{ url('/quality-assurance') }}"><i class="fa fa-check iconmargin-set" aria-hidden="true"></i>
+                Quality</a>
+        </div>
+        <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'equipment')?'active':'' }}">
+            <a href="{{ url('/equipment') }}"><i class="fa fa-cogs iconmargin-set" aria-hidden="true"></i> Equipment</a>
+        </div>
+        <div
+            class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'rabies-test')?'active':'' }}">
+            <a href="{{ url('/rabies-test') }}"><i class="fa fa-registered iconmargin-set" aria-hidden="true"></i>
+                Rabies</a>
+        </div>
+        <div
+            class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'expenditure')?'active':'' }}">
+            <a href="{{ url('/expenditure') }}"><i class="fa fa-expand iconmargin-set" aria-hidden="true"></i>
+                Expenditure</a> </i>
+        </div>
+        <div
+            class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'report-list')?'active':'' }}">
+            <a href="{{ url('/report-list') }}"><i class="fa fa-file iconmargin-set" aria-hidden="true"></i> Reports</a>
+            </i>
+        </div>
         @endif
         @if (Auth::user()->user_type == 1)
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'dashboard')?'active':'' }}">
-                <a href="{{ url('/dashboard') }}">Dashboard</a> </i>
+        <!-- ************** -->
+        <div class="sidebarAccordion">
+
+            <div
+                class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'dashboard')?'active':'' }}">
+                <a class="accordion-heading" data-toggle="collapse" href="#multiCollapseExample1" role="button"
+                    aria-expanded="false" aria-controls="multiCollapseExample1">Rabies Dashboard </a>
+
+
+
             </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'pform')?'active':'' }}">
-                <a href="{{ url('/pform') }}">P Form</a> </i>
+
+            <div class="row">
+                <div class="col">
+                    <div class="collapse multi-collapse" id="multiCollapseExample1">
+                        <div class="card card-body">
+                            <div
+                                class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'dashboard')?'active':'' }}">
+                                <a href="{{ url('/dashboard') }}">Dashboard</a> </i>
+                            </div>
+                            <div
+                                class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'pform')?'active':'' }}">
+                                <a href="{{ url('/pform') }}">P Form</a> </i>
+                            </div>
+                            <div
+                                class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'sform')?'active':'' }}">
+                                <a href="{{ url('/sform') }}">S Form</a> </i>
+                            </div>
+                            <div
+                                class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'lform')?'active':'' }}">
+                                <a href="{{ url('/lform') }}">L Form</a> </i>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+
             </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'sform')?'active':'' }}">
-                <a href="{{ url('/sform') }}">S Form</a> </i>
-            </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::segment(1) == 'lform')?'active':'' }}">
-                <a href="{{ url('/lform') }}">L Form</a> </i>
-            </div>
-            <div class="link bg-primary text-white dashboard-title {{ (Request::routeIs('laboratory-dashboard') == 'laboratory-dashboard')?'active':'' }}">
-                <a href="{{ url('/laboratory-dashboard') }}">Laboratory Dashboard</a> </i>
-            </div>
+            
+        </div>
+        <div
+            class="link bg-primary text-white dashboard-title {{ (Request::routeIs('laboratory-dashboard') == 'laboratory-dashboard')?'active':'' }}">
+            <a href="{{ url('/laboratory-dashboard') }}">Laboratory Dashboard</a> </i>
+        </div>
+        <!-- ********************* -->
+
         @endif
         <!-- <div class="link bg-primary text-white dashboard-title">
             <a href="{{ url('/dashboard') }}">Dashboard</a> </i>
         </div> -->
     </li>
     @if (env('SIDE_BAR_SHOW') != '0')
-        <!-- <li>
+    <!-- <li>
             <div class="link"> <i class="far fa-circle nav-icon"></i>pForm<i class="fa fa-chevron-down"></i></div>
             <ul class="submenu">
                 <li><a href="{{ url('pformDashboard') }}" class="{{ Request::routeIs('pform2') ? 'active' : '' }}">Dashboard</a></li>
@@ -112,33 +153,33 @@
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> -->
 
 <script>
-    // $(function() {
-    //     var Accordion = function(el, multiple) {
-    //         this.el = el || {};
-    //         this.multiple = multiple || false;
+// $(function() {
+//     var Accordion = function(el, multiple) {
+//         this.el = el || {};
+//         this.multiple = multiple || false;
 
-    //         // Variables privadas
-    //         var links = this.el.find('.link');
-    //         // Evento
-    //         links.on('click', {
-    //             el: this.el,
-    //             multiple: this.multiple
-    //         }, this.dropdown)
-    //     }
+//         // Variables privadas
+//         var links = this.el.find('.link');
+//         // Evento
+//         links.on('click', {
+//             el: this.el,
+//             multiple: this.multiple
+//         }, this.dropdown)
+//     }
 
-    //     Accordion.prototype.dropdown = function(e) {
-    //         var $el = e.data.el;
-    //         $this = $(this),
-    //             $next = $this.next();
+//     Accordion.prototype.dropdown = function(e) {
+//         var $el = e.data.el;
+//         $this = $(this),
+//             $next = $this.next();
 
-    //         $next.slideToggle();
-    //         $this.parent().toggleClass('open');
+//         $next.slideToggle();
+//         $this.parent().toggleClass('open');
 
-    //         if (!e.data.multiple) {
-    //             $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
-    //         };
-    //     }
+//         if (!e.data.multiple) {
+//             $el.find('.submenu').not($next).slideUp().parent().removeClass('open');
+//         };
+//     }
 
-    //     var accordion = new Accordion($('#accordion'), false);
-    // });
+//     var accordion = new Accordion($('#accordion'), false);
+// });
 </script>
