84d26762d5ca7f3e7db0725edf4b876c1df03357
Echt Tech <rahul.chaurasia@echttech.com>
2024-03-20 11:49:47 +0530

    bug_fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,9 +3,6 @@
 {{ __('NRCP Dashboard') }}
 @endsection
 @section('content')
-<link rel="stylesheet" href="{{ asset('assets/css/filter_multi_select.css') }}">
-
-
 <div class="container-fluid">
     <!-- Info boxes -->
     <div class="row">
@@ -84,13 +81,13 @@
                                                             echo "<option value='$year'>$year</option>";
                                                         }
                                                         ?>
-                                                </select>
-                                                <!-- <span class="calender"><i class="fa fa-calendar" aria-hidden="true"></i>
+                                                    </select>
+                                                    <!-- <span class="calender"><i class="fa fa-calendar" aria-hidden="true"></i>
                                                     </span> -->
-                                                <small id="fromYear-error" class="form-text text-muted">
-                                                </small>
+                                                    <small id="fromYear-error" class="form-text text-muted">
+                                                    </small>
+                                                </div>
                                             </div>
-                                        </div>
 
                                             <div class="col-lg-2 col-md-2 col-4">
                                                 <div class="form-group">
@@ -101,9 +98,9 @@
                                                     </select>
                                                     <!-- <span class="calender"><i class="fa fa-calendar" aria-hidden="true"></i>
                                                     </span> -->
-                                                <small id="toYear-error" class="form-text text-muted"></small>
+                                                    <small id="toYear-error" class="form-text text-muted"></small>
+                                                </div>
                                             </div>
-                                                    </div>
                                             <div class="col-lg-2 col-md-2 col-4">
                                                 <div class="form-group">
                                                     <label for="formType">Form Type<span class="star"></span></label>
@@ -134,14 +131,13 @@
                                                     </small>
                                                 </div>
                                             </div>
-                                        </div>
 
 
                                             <div class="col-lg-12 col-md-2 col-4" id="test_performed">
                                                 <div class="form-group">
                                                     <label for="testPerformed" class="d-block">Test Performed<span
                                                             class="star">*</span></label>
-                                                    <select class="form-control" name="test_performed" id="mySelect2" multiple="multiple"
+                                                    <select class="form-control" id="mySelect2" multiple="multiple"
                                                         aria-label="Default select" onKeyPress = "handleTest(event)">
                                                         <!-- <option value="" selected>--All--</option> -->
                                                         <option name="test-performed" value="direct_fat_post">Direct
@@ -173,7 +169,7 @@
                                                     <button id="apply_filter"
                                                         class="btn  bg-primary text-light apply-filter button border-0 mr-2">Search</button>
 
-                                            </div>
+                                                </div>
 
                                                 <input type="hidden" value="" id="filter_state">
                                                 <input type="hidden" value="" id="filter_district">
@@ -269,28 +265,10 @@
                                             </div>
                                         </div>
                                     </div>
-                                </div>
 
 
-                                <!-- /.row -->
-                                <div class="card-body p-3 my-5 dashboard-filter">
-                                    <div class="row bg-white">
-                                        <div class="col-md-12 ">
-                                            <div
-                                                class="map-text m-0 mb-2 d-flex align-items-center justify-content-between">
-                                                <h1 class="m-0 mr-3 d-inline-block">Deaths cases state wise </h1>
-                                                <button class="buttons-print float-right" type="button"
-                                                    onclick="printDiv('dashboardMap')"><span> <i
-                                                            class="fa fa-print"></i></span></button>
-                                            </div>
-
-                                        </div>
-
-                                        <!-- <div class="col-md-6 d-flex justify-content-end">
-                                            <button class="buttons-print" type="button"><span> <i class="fa fa-print"></i></span></button>
-                                            </div> -->
-                                    </div>
-                                    <div>
+                                    <!-- /.row -->
+                                    <div class="card-body p-3 my-5 dashboard-filter">
                                         <div class="row bg-white">
                                             <div class="col-md-12 ">
                                                 <div
@@ -300,7 +278,6 @@
                                                         onclick="printDiv('dashboardMap')"><span> <i
                                                                 class="fa fa-print"></i></span></button>
                                                 </div>
-
 
                                             </div>
 
@@ -420,63 +397,23 @@
                                                     <div id="barchart_materialcase" style="width: 100%;">
                                                     </div>
                                                 </div>
-
-
                                             </div>
-                                        </div>
-                                    </div>
 
-                                </div>
-
-                                <!-- /.row -->
-                                <!-- graph start-->
-                                <div id="graphical_view">
-                                    <div class="row">
-                                        <div class="col-md-6 pr-4">
-                                            <div id="containerPie" class="piechart dashboard-filter" height="400">
-                                            </div>
-                                        </div>
-
-                                        <div class="col-md-6 pl-4">
-                                            <div id="containerPie2nd" class="piechart dashboard-filter " height="400">
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="row">
-                                        <div class="col-md-6 pr-4">
-                                            <div id="chart" class="dashboard-filter mt-5"></div>
-                                        </div>
-
-                                        <div class="col-md-6 pl-4">
-                                            <div class="dashboard-filter mt-5">
-                                                <h1>Cases by Age Group in India (Based on Male and Female)</h1>
-                                                <div id="chartContainer" style="height: 400px;"></div>
-                                            </div>
-                                        </div>
-
-                                        <div class="col-md-6 pr-4">
-                                            <div class="dashboard-filter mt-5">
-
-                                                <div id="barchart_materialcase" style="width: 100%;">
+                                            <div class="col-md-6 pl-4">
+                                                <div class="dashboard-filter mt-5">
+                                                    {{-- <h1></h1> --}}
+                                                    <div id="barchart_materialdeaths" style="width: 100%;">
+                                                    </div>
                                                 </div>
                                             </div>
+
+
+
                                         </div>
-
-                                        <div class="col-md-6 pl-4">
-                                            <div class="dashboard-filter mt-5">
-                                                {{-- <h1></h1> --}}
-                                                <div id="barchart_materialdeaths" style="width: 100%;">
-                                                </div>
-                                            </div>
-                                        </div>
-
-
-
                                     </div>
-                                </div>
-                                <!-- end here -->
+                                    <!-- end here -->
 
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -485,8 +422,4 @@
             @endif
         </div>
     </div>
-    <script src="{{ asset('assets/js/filter-multi-select-bundle.min.js') }}"></script>
-    <script>
-    var mySelect2 = $('#mySelect2').filterMultiSelect();
-    </script>
-    @endsection
+@endsection
